ab3e0e2ef384faf55f1a0eaa0c35bcea39ba7b3d
Josh Buker <crypto@joshbuker.com>
2021-04-28 17:51:28 -0700

    Post python version of Carl's code

---
+++ projects/rc_maqueen/microbit_controller/receiver.py
@@ -0,0 +1,49 @@
+def on_received_number(receivedNumber):
+    global received
+    received = receivedNumber
+    if receivedNumber == 0:
+        DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CW, 128)
+        basic.show_leds("""
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            """)
+    else:
+        if receivedNumber == 1:
+            DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CW, 128)
+            basic.show_leds("""
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                """)
+        else:
+            if receivedNumber == 2:
+                DFRobotMaqueenPlus.motot_run(Motors.ALL, Dir.CW, 128)
+                basic.show_leds("""
+                    . . # . .
+                    . # # # .
+                    # . # . #
+                    . . # . .
+                    . . # . .
+                    """)
+            else:
+                if receivedNumber == 4:
+                    DFRobotMaqueenPlus.motot_run(Motors.ALL, Dir.CCW, 128)
+                    basic.show_leds("""
+                        . . # . .
+                        . . # . .
+                        # . # . #
+                        . # # # .
+                        . . # . .
+                        """)
+                else:
+                    DFRobotMaqueenPlus.motot_run(Motors.ALL, Dir.CW, 0)
+                    basic.show_icon(IconNames.NO)
+radio.on_received_number(on_received_number)
+
+received = 0
+radio.set_group(90)
---
+++ projects/rc_maqueen/microbit_controller/transmitter.py
@@ -0,0 +1,46 @@
+radio.set_group(90)
+
+def on_forever():
+    if input.acceleration(Dimension.X) > 700:
+        radio.send_number(0)
+        basic.show_leds("""
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            """)
+    else:
+        if input.acceleration(Dimension.X) < -700:
+            radio.send_number(1)
+            basic.show_leds("""
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                """)
+        else:
+            if input.acceleration(Dimension.Y) < -700:
+                radio.send_number(2)
+                basic.show_leds("""
+                    . . # . .
+                    . # # # .
+                    # . # . #
+                    . . # . .
+                    . . # . .
+                    """)
+            else:
+                if input.acceleration(Dimension.Y) > 700:
+                    radio.send_number(4)
+                    basic.show_leds("""
+                        . . # . .
+                        . . # . .
+                        # . # . #
+                        . # # # .
+                        . . # . .
+                        """)
+                else:
+                    radio.send_number(3)
+                    basic.show_icon(IconNames.NO)
+basic.forever(on_forever)
