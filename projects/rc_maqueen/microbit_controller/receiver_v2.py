603acabca64432b19c4686eada34fd136dbcc25a
Josh Buker <crypto@joshbuker.com>
2021-04-28 20:15:09 -0700

    Microbit controller v2

--- projects/rc_maqueen/microbit_controller/receiver_v2.py
+++ projects/rc_maqueen/microbit_controller/receiver_v2.py
@@ -1,10 +1,174 @@
+# ---------------
+# Hardware Values
+# ---------------
+
+RADIO_GROUP = 90
+RADIO_DEADZONE = 200
+
+LEFT_MOTOR = Motors.M1
+RIGHT_MOTOR = Motors.M2
+
+FORWARD = Dir.CW
+REVERSE = Dir.CCW
+
+MAX_SPEED = 255
+
+# -----------
+# Code Begins
+# -----------
+
 straight_channel = 0
 turn_channel = 0
+left_motor_speed = 0
+right_motor_speed = 0
+
+def constrain(val, min_val, max_val):
+    return min(max_val, max(min_val, val))
+
+def translate(value, left_min, left_max, right_min, right_max):
+    left_span = left_max - left_min
+    right_span = right_max - right_min
+    # Convert the left range into a 0-1 range float
+    value_scaled = (value - left_min) / left_span
+    # Convert the 0-1 range into a value in the right range
+    return right_min + (value_scaled * right_span)
 
 def on_received_value(name, value):
     global straight_channel
     global turn_channel
     if name == 'straight':
         straight_channel = value
-    else if name == 'turn':
+    elif name == 'turn':
         turn_channel = value
+
+def radio_to_speed(radio_value):
+    speed = translate(radio_value, 200, 1000, -MAX_SPEED, MAX_SPEED)
+    speed = constrain(speed, -MAX_SPEED, MAX_SPEED)
+    return speed
+
+# Lazy implementation of mixing
+def calculate_speeds_from_radio():
+    global left_motor_speed
+    global right_motor_speed
+    # Reset to 0
+    left_motor_speed = 0
+    right_motor_speed = 0
+    # Forward/Backward
+    if abs(straight_channel) > RADIO_DEADZONE:
+        left_motor_speed = radio_to_speed(straight_channel)
+        right_motor_speed = radio_to_speed(straight_channel)
+    # Left/Right
+    if abs(turn_channel) > RADIO_DEADZONE:
+        left_motor_speed += radio_to_speed(turn_channel)
+        right_motor_speed -= radio_to_speed(turn_channel)
+    # Constrain motors to max speed when mixing
+    left_motor_speed = constrain(left_motor_speed, -MAX_SPEED, MAX_SPEED)
+    right_motor_speed = constrain(right_motor_speed, -MAX_SPEED, MAX_SPEED)
+
+def set_motor_speeds():
+    if left_motor_speed > 0:
+        DFRobotMaqueenPlus.motot_run(LEFT_MOTOR, FORWARD, left_motor_speed)
+    elif left_motor_speed < 0:
+        DFRobotMaqueenPlus.motot_run(LEFT_MOTOR, REVERSE, abs(left_motor_speed))
+    else:
+        DFRobotMaqueenPlus.motot_stop(LEFT_MOTOR)
+
+    if right_motor_speed > 0:
+        DFRobotMaqueenPlus.motot_run(RIGHT_MOTOR, FORWARD, right_motor_speed)
+    elif right_motor_speed < 0:
+        DFRobotMaqueenPlus.motot_run(RIGHT_MOTOR, REVERSE, abs(right_motor_speed))
+    else:
+        DFRobotMaqueenPlus.motot_stop(RIGHT_MOTOR)
+
+def direction_arrow():
+    if straight_channel > RADIO_DEADZONE:
+        if turn_channel > RADIO_DEADZONE:
+            basic.show_leds("""
+                            . # # # #
+                            . . . # #
+                            . . # . #
+                            . # . . #
+                            # . . . .
+                            """)
+        elif turn_channel < -RADIO_DEADZONE:
+            basic.show_leds("""
+                            # # # # .
+                            # # . . .
+                            # . # . .
+                            # . . # .
+                            . . . . #
+                            """)
+        else:
+            basic.show_leds("""
+                            . . # . .
+                            . # # # .
+                            # . # . #
+                            . . # . .
+                            . . # . .
+                            """)
+    elif straight_channel < -RADIO_DEADZONE:
+        if turn_channel > RADIO_DEADZONE:
+            basic.show_leds("""
+                            # . . . .
+                            . # . . #
+                            . . # . #
+                            . . . # #
+                            . # # # #
+                            """)
+        elif turn_channel < -RADIO_DEADZONE:
+            basic.show_leds("""
+                            . . . . #
+                            # . . # .
+                            # . # . .
+                            # # . . .
+                            # # # # .
+                            """)
+        else:
+            basic.show_leds("""
+                            . . # . .
+                            . . # . .
+                            # . # . #
+                            . # # # .
+                            . . # . .
+                            """)
+    else:
+        if turn_channel > RADIO_DEADZONE:
+            basic.show_leds("""
+                            . . # . .
+                            . . . # .
+                            # # # # #
+                            . . . # .
+                            . . # . .
+                            """)
+        elif turn_channel < -RADIO_DEADZONE:
+            basic.show_leds("""
+                            . . # . .
+                            . # . . .
+                            # # # # #
+                            . # . . .
+                            . . # . .
+                            """)
+        else:
+            basic.show_leds("""
+                            . # # # .
+                            # . . . #
+                            # . . . #
+                            # . . . #
+                            . # # # .
+                            """)
+
+
+def setup():
+    basic.show_icon(IconNames.HEART)
+    radio.on_received_value(on_received_value)
+    radio.set_group(RADIO_GROUP)
+    DFRobotMaqueenPlus.i2c_init()
+
+def loop():
+    calculate_speeds_from_radio()
+    set_motor_speeds()
+    direction_arrow()
+    # basic.pause(25) # Keep the refresh rate sane
+
+setup()
+basic.forever(loop)
--- projects/rc_maqueen/microbit_controller/transmitter_v2.py
+++ projects/rc_maqueen/microbit_controller/transmitter_v2.py
@@ -1,53 +1,103 @@
-DEADZONE = 100
+DEADZONE = 200
+RADIO_GROUP = 90
 
 def setup():
-    radio.set_group(90)
+    radio.set_group(RADIO_GROUP)
+    basic.show_icon(IconNames.HEART)
 
-def show_leds():
-    if input.acceleration(Dimension.X) > DEADZONE:
-        basic.show_leds("""
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            """)
-    else:
-        if input.acceleration(Dimension.X) < -DEADZONE:
+def direction_arrow():
+    straight = -input.acceleration(Dimension.Y)
+    turn = input.acceleration(Dimension.X)
+    if straight > DEADZONE:
+        if turn > DEADZONE:
             basic.show_leds("""
-                . . # . .
-                . # . . .
-                # # # # #
-                . # . . .
-                . . # . .
-                """)
+                            . # # # #
+                            . . . # #
+                            . . # . #
+                            . # . . #
+                            # . . . .
+                            """)
+        elif turn < -DEADZONE:
+            basic.show_leds("""
+                            # # # # .
+                            # # . . .
+                            # . # . .
+                            # . . # .
+                            . . . . #
+                            """)
         else:
-            if input.acceleration(Dimension.Y) < -DEADZONE:
-                basic.show_leds("""
-                    . . # . .
-                    . # # # .
-                    # . # . #
-                    . . # . .
-                    . . # . .
-                    """)
-            else:
-                if input.acceleration(Dimension.Y) > DEADZONE:
-                    basic.show_leds("""
-                        . . # . .
-                        . . # . .
-                        # . # . #
-                        . # # # .
-                        . . # . .
-                        """)
-                else:
-                    basic.show_icon(IconNames.NO)
+            basic.show_leds("""
+                            . . # . .
+                            . # # # .
+                            # . # . #
+                            . . # . .
+                            . . # . .
+                            """)
+    elif straight < -DEADZONE:
+        if turn > DEADZONE:
+            basic.show_leds("""
+                            # . . . .
+                            . # . . #
+                            . . # . #
+                            . . . # #
+                            . # # # #
+                            """)
+        elif turn < -DEADZONE:
+            basic.show_leds("""
+                            . . . . #
+                            # . . # .
+                            # . # . .
+                            # # . . .
+                            # # # # .
+                            """)
+        else:
+            basic.show_leds("""
+                            . . # . .
+                            . . # . .
+                            # . # . #
+                            . # # # .
+                            . . # . .
+                            """)
+    else:
+        if turn > DEADZONE:
+            basic.show_leds("""
+                            . . # . .
+                            . . . # .
+                            # # # # #
+                            . . . # .
+                            . . # . .
+                            """)
+        elif turn < -DEADZONE:
+            basic.show_leds("""
+                            . . # . .
+                            . # . . .
+                            # # # # #
+                            . # . . .
+                            . . # . .
+                            """)
+        else:
+            basic.show_leds("""
+                            . # # # .
+                            # . . . #
+                            # . . . #
+                            # . . . #
+                            . # # # .
+                            """)
 
 def send_direction():
-    radio.send_value('straight', input.acceleration(Dimension.X))
-    radio.send_value('turn', input.acceleration(Dimension.Y))
+    straight = -input.acceleration(Dimension.Y)
+    turn = input.acceleration(Dimension.X)
+    if abs(straight) > DEADZONE:
+        radio.send_value('straight', straight)
+    else:
+        radio.send_value('straight', 0)
+    if abs(turn) > DEADZONE:
+        radio.send_value('turn', turn)
+    else:
+        radio.send_value('turn', 0)
 
 def loop():
-    show_leds()
+    direction_arrow()
     send_direction()
 
 basic.forever(loop)
